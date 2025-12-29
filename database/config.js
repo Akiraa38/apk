082ecc18d965a90f484495a1305ea42879fe9931
Akiraa38 <azmil.bocil12@gmail.com>
2025-12-29 16:18:58 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8361322738:AAE_cp56ZjYs4_GLWQlVd4q4KH6PSE9YV4s",
+  owners: ["7885097329"],
+  ipvps: "https://pnl-free-hamz.ptero-hamz.my.id", 
+  port: 1502
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 2323-2324 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
