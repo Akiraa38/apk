202451788764f2eacaf72409f41a9607f88931de
Akiraa38 <azmil.bocil12@gmail.com>
2025-12-29 16:10:36 +0700

    Delete database directory

--- database/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  tokens: "8223005114:AAGlcl3RYjMwjAeJOCYR8omPH5GMvP8JHKY",  // Ubah Jadi Token Bot Mu !!!
-  owner: "7885097329", // Ubah Jadi Id Mu !!!
-  port: "3089", // Ubah Jadi Port Panel Mu !!!
-  ipvps: "http://pirzypublikvtwo.anti-ddos.me" // Ubah Jadi Ip Vps Mu !!!
-};
